40d739668be48969d2e14d3683f6fa68a9c5c859
Robert Dias <102939474+Robert-dvk@users.noreply.github.com>
2025-08-07 11:22:26 -0300

    Initial commit

---
+++ src/main/java/avmb/desafio/AstenTask/AstenTaskApplication.java
@@ -0,0 +1,13 @@
+package avmb.desafio.AstenTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstenTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstenTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/avmb/desafio/AstenTask/AstenTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package avmb.desafio.AstenTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstenTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
